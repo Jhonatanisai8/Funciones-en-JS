45801510d03bfa82c249bb23d856316f232d1b11
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-19 10:27:05 -0500

    02 sumar dos numeros

---
+++ exercies/02SumaNumeros.js
@@ -0,0 +1,8 @@
+/*Crea una función que reciba dos números 
+y devuelva su suma.*/
+
+function sumarNumeros(numero01, numero02) {
+    return numero01 + numero02;
+}
+
+console.log(`la suma es ${sumarNumeros(12, 34)} `)
